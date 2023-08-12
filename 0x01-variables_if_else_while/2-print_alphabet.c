44b04921def12dd90370ab9210f3a61cf46177dc
Rahmat <rahmatbola3@gmail.com>
2023-08-12 05:37:36 -0700

    alphabets script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Describtion: Printing the lowercase alphabets
+ *
+ * Return: Always 0 (sucess)
+ *
+ */
+
+int main(void)
+
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+
+	{
+	putchar(letter);
+	letter++;
+	}
+
+	return (0);
+}
