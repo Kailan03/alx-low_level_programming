6d6bbe4e75f401f6499bcfc092360c942877c79e
Rahmat <rahmatbola3@gmail.com>
2023-08-13 08:08:21 -0700

    digit and alphabet script

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	for (d = 'a'; d <= 'f'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
