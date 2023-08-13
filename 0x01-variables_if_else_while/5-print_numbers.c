c698f429f1c79927c4db33d0e2ce6182c1132502
Rahmat <rahmatbola3@gmail.com>
2023-08-13 02:25:56 -0700

    numberz in base 10 script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+		putchar(i++);
+	}
+		putchar('\n');
+
+	return (0);
+}
