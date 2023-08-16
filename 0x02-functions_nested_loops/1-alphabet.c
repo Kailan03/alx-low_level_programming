81924095a7708ea0a0e81ff20bebffb1881bc61c
Rahmat <rahmatbola3@gmail.com>
2023-08-16 15:01:10 -0700

    putchar & alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print all alphabets in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	_putchar(l);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,18 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-//functions prototypes
-{
-int main(void)
-	int void print_alphabet(void);
-int void print_alphabet_x10(void);
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-int void Jack-Bauer(void);
-int void times_table(void);
-int add(int, int);int void print_to_98(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-return (0);
-}
+#endif
