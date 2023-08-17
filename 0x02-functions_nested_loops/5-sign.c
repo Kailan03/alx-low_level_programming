e22efd10cdfeab972c44023508af30c5d544cb8d
Rahmat <rahmatbola3@gmail.com>
2023-08-16 17:48:18 -0700

    abs program

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign - prints the number sign
+ * @n: the int to check
+ * Return: 1 
  */
-int main(void)
+int print_sign(void)
 {
-    int r;
-    
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	if (n > 0)
+		_putchar('+');
+	return (1);
+	else if (n == 0)
+		_putchar(48);
+			return (0);
+	else if (n < 0)
+		_putchar('-');
+	return (-1);
 }
