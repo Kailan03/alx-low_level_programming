5a4e91ad42dd501331e4f3ad0a978aaa90b50c3a
Rahmat <rahmatbola3@gmail.com>
2023-09-21 15:10:16 -0700

    singly_linked_lists project

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - returns number of elements in list_t
+ * @h: pointer tp list_t
+ *
+ * Return: number of elements in list_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
+
